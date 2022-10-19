a1480d8b1d1977a5db7cd3b9b13be3083bd6bd6e
Cassandra Keddis <cassandra@thinkific.com>
2022-10-18 19:48:55 -0700

    implementing wasd

--- input.js
+++ input.js
@@ -1,6 +1,9 @@
 // setup interface to handle user input from stdin
+const { connect } = require('./client')
+let connection;
 
-const setupInput = function () {
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -14,6 +17,14 @@ const handleUserInput = function (data) {
   const stdin = process.stdin;
   if (data === '\x03') {
     process.exit();
+  } else if (data === 'w') {
+    connection.write('Move: up');
+  } else if (data === 'a') {
+    connection.write('Move: left');
+  } else if (data === 's') {
+    connection.write('Move: down');
+  } else if (data === 'd') {
+    connection.write('Move: right');
   }
 };
 
